838ecb79a661caf73a5274211a775e75513c8baa
Magnus Fredheim Hanssen <mha430@post.uit.no>
2022-06-01 21:30:30 +0200

    Final commit

--- include/ColSphereBody.hpp
+++ include/ColSphereBody.hpp
@@ -16,16 +16,9 @@ public:
         if(!target->active)
             return false;
         glm::vec3 posA = this->getWorldPos();
-        //std::cout << posA.x << std::endl;
         glm::vec3 posB = target->getWorldPos();
-        //std::cout << posB.x << std::endl;
         float radA = this->getRadius();
-        //std::cout << radA << std::endl
         float radB = target->getRadius();
-        //std::cout << radB << std::endl
-        //float minDistance =  radA + radB;
-        //std::cout << minDistance << std::endl;
-        //std::cout << glm::distance(posA, posB) << std::endl;
 
         return radA + radB > glm::distance(posA, posB);
     }
